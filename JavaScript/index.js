46a73612ea3ba090317dc9edaa7632d1dbe9aa27
Rinka Is Developing lol <77236422+RinkaGI@users.noreply.github.com>
2022-12-10 22:25:31 +0100

    Update index.js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,6 +1,6 @@
 const Lightcord = require('./src/lightcord');
 
-const bot = new Lightcord.Bot('MTAwNjE4NTcyNTQyMDY1NDU5Mg.GEkX77.cyUdL38URtcWdti43jSfA-IQVAJwzQjTdzYR4M', Lightcord.Intents.Minimum)
+const bot = new Lightcord.Bot('Token', Lightcord.Intents.Minimum)
 
 bot.on(Lightcord.Events.Ready, () => {
     console.log('Ready with event emitter.')
